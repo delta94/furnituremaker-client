c5f4fe4da1e3699b86ee4c8ae36d250beea96836
kenkz447 <kenkz447@gmail.com>
2018-08-01 23:43:24 +0700

    update restore-git-submodules script

--- restore-git-submodules.sh
+++ restore-git-submodules.sh
@@ -7,5 +7,6 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     do
         url_key=$(echo $path_key | sed 's/\.path/.url/')
         url=$(git config -f .gitmodules --get "$url_key")
+        git rm -r $path
         git submodule add $url $path
     done
