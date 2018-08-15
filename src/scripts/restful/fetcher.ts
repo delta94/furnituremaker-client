c109b4cea323d5760cbf82be4d1bcfc125bdbc75
kenkz447 <kenkz447@gmail.com>
2018-08-15 14:35:25 +0700

    update

--- src/scripts/restful/fetcher.ts
+++ src/scripts/restful/fetcher.ts
@@ -1,48 +1,16 @@
-import { Fetcher, Resource, ResourceParameter } from 'react-restful';
+import { Fetcher } from 'react-restful';
 
 import { getToken } from '@/configs';
 
 import { restfulStore } from './store';
 
 class RestfulFetcher extends Fetcher {
-    readonly createDefaultRequestInit = () => ({ headers: new Headers() });
-
-    fetch(url: string, requestInit: RequestInit) {
+    async beforeFetch(url: string, requestInit: RequestInit) {
         const token = getToken();
         if (token && requestInit.headers instanceof Headers) {
             requestInit.headers.append('Authorization', `Bearer ${token}`);
         }
-
-        return fetch(url, requestInit);
-    }
-
-    async fetchResource<DataModel>(resource: Resource<DataModel>, params: ResourceParameter[]) {
-        try {
-            const url = resource.urlReslover(params);
-            const requestInit: RequestInit = resource.requestInitReslover(params) || this.createDefaultRequestInit();
-            requestInit.method = resource.method;
-
-            const response = await this.fetch(url, requestInit);
-
-            if (!response.ok) {
-                throw response;
-            }
-
-            const responseContentType = response.headers.get('content-type');
-            if (responseContentType && responseContentType.startsWith('application/json')) {
-                const json = await response.json();
-                if (resource.mapDataToStore) {
-                    resource.mapDataToStore(json, resource.recordType, this.store);
-                }
-                return json;
-            }
-            return await response.text();
-        } catch (error) {
-            if (error instanceof Response) {
-                throw error;
-            }
-            throw new Error(error);
-        }
+        return requestInit;
     }
 }
 
