0c6e6ba5ea2d8ed49ac906524119cd9ae24300f9
kenkz447 <kenkz447@gmail.com>
2018-12-10 20:49:49 +0700

    add service worker registation

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,22 @@ if (module.hot) {
         nextStartup();
     });
 }
+
+if (process.env.NODE_ENV === 'production') {
+    if ('serviceWorker' in navigator) {
+        const workerUrl = '/static/service-worker.js';
+        const workerOptions = { scope: '/' };
+        const loadServiceWorker = async () => {
+            try {
+                const registration = await navigator.serviceWorker.register(workerUrl, workerOptions);
+                // tslint:disable-next-line:no-console
+                console.info('SW registered: ', registration);
+            } catch (registrationError) {
+                // tslint:disable-next-line:no-console
+                console.info('SW registration failed: ', registrationError);
+            }
+        };
+
+        window.addEventListener('load', loadServiceWorker);
+    }
+}
